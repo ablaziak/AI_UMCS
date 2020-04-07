c6a5e355e378f7ae426a0744bf5c8d526b638401
ablaziak <63005095+ablaziak@users.noreply.github.com>
2020-04-07 19:56:06 +0200

    poprawiona wersja

--- first_neural_network — kopia/first_neural_network.py
+++ first_neural_network — kopia/first_neural_network.py
@@ -1,84 +1,105 @@
-import random
+##import random
 from math import exp
+import numpy as np
+
 
 class Neuron:
-    w0 = random.random()
-    w1 = random.random()
-    w2 = random.random()
-    w3 = random.random()
-    w4 = random.random()
-    w5 = random.random()
+    w0 = np.random.normal()
+    w1 = np.random.normal()
+    w2 = np.random.normal()
+    w3 = np.random.normal()
+    w4 = np.random.normal()
+    w5 = np.random.normal()
 
-    b0 = random.random()
-    b1 = random.random()
-    b2 = random.random()
+    b0 = np.random.normal()
+    b1 = np.random.normal()
+    b2 = np.random.normal()
 
     def sigmoid(self, x):
-        return 1/(1+exp(-x))
+        return 1 / (1 + exp(-x))
 
     def derivsigmoid(self, x):
-        return self.sigmoid(self, x)*(1-self.sigmoid(self, x))
+        return self.sigmoid(self, x) * (1 - self.sigmoid(self, x))
 
-    def mse_loss(self, y, ypred):
-        return ((y-ypred)**2)/2
+    def mse(self, target_value, y):
+        return ((target_value - y) ** 2) / 2
 
-    def derivmse_loss(self, y, ypred):
-        return -2*(y-ypred)
+    def derivmse(self, target_value, y):
+        return -2 * (target_value - y)
 
     def feedforward(self, x):
-        h0 = self.sigmoid(self, self.w0*x[0]+self.w1*x[1]+self.b0)
+        h0 = self.sigmoid(self, self.w0 * x[0] + self.w1 * x[1] + self.b0)
         h1 = self.sigmoid(self, self.w2 * x[0] + self.w3 * x[1] + self.b1)
         o0 = self.sigmoid(self, self.w4 * h0 + self.w5 * h1 + self.b2)
         return o0
 
-    def fsum_o0(self, x):
-        h0 = self.sigmoid(self, self.w0*x[0]+self.w1*x[1]+self.b0)
-        h1 = self.sigmoid(self, self.w2 * x[0] + self.w3 * x[1] + self.b1)
-        return (self.w4 * h0 + self.w5 * h1 + self.b2)
+    def printall(self, data, result, loss):
+        for i in range (0,4):
+            print("[", data[i][0], ", ", data[i][1], "]", end=' | ')
+        print()
+        for i in range(0,4):
+            print("%.8f" %result[i], end=' | ')
+        print()
+        for i in range(0,4):
+            print("loss: %.3f" %loss[i], end=' |')
+        print("\n")
 
-    def train(self, data, ypred, q, epochs):
+
+    def train(self, data, target_value, learn_rate, epochs):
         result = [0, 0, 0, 0]
+        loss = [0, 0, 0, 0]
         for j in range(0, epochs):
-            for i in range (0,4):
-                result[i] = self.feedforward(self, data[i])
-                sum_o0 = self.fsum_o0(self, data[i])
+            for i in range(0, 4):
+                sum_h0 = self.w0 * data[i][0] + self.w1 * data[i][1] + self.b0
+                sum_h1 = self.w2 * data[i][0] + self.w3 * data[i][1] + self.b1
 
-                mse = self.mse_loss(self, result[i], ypred[i])
-                deriv_mse = self.derivmse_loss(self, result[i], ypred[i])
-                
-                # POCHODNA NIE IDZIE Z RESULT TYLKO POWINIENES SOBIE SUME POLICZYC I JA ZAPAMIETAC I POCHODNA IDZIE Z SUMY NP. Z w5*h1 + w6*h2 +b3, A TY ROBISZ Z WARTOSCI WZMOCNIONEJ FUNKCJA AKTYWACJI
-                dw5 = (self.w2 * data[i][0] + self.w3 * data[i][1] + self.b1) * self.derivsigmoid(self, sum_o0)
-                dw4 = (self.w0 * data[i][0] + self.w1 * data[i][1] + self.b0) * self.derivsigmoid(self, sum_o0)
+                h0 = self.sigmoid(self, sum_h0)
+                h1 = self.sigmoid(self, sum_h1)
+
+                sum_o0 = self.w4 * h0 + self.w5 * h1 + self.b2
+                o0 = self.sigmoid(self, sum_o0)
+
+                ## mse = self.mse_loss(self, result[i], ypred[i])
+
+                deriv_mse = self.derivmse(self, target_value[i], o0)
+
+                dw5 = h1 * self.derivsigmoid(self, sum_o0)
+                dw4 = h0 * self.derivsigmoid(self, sum_o0)
                 db2 = self.derivsigmoid(self, sum_o0)
+
                 dh0 = dw4 * self.derivsigmoid(self, sum_o0)
                 dh1 = dw5 * self.derivsigmoid(self, sum_o0)
-                
-                #TUTAJ DOBRZE POCHODNA Z SUMY WAZONEJ
-                dw0 = self.derivsigmoid(self, self.w0 * data[i][0] + self.w1 * data[i][1] + self.b0) * data[i][0]
-                dw1 = self.derivsigmoid(self, self.w0 * data[i][0] + self.w1 * data[i][1] + self.b0) * data[i][1]
-                dw2 = self.derivsigmoid(self, self.w2 * data[i][0] + self.w3 * data[i][1] + self.b1) * data[i][0]
-                dw3 = self.derivsigmoid(self, self.w2 * data[i][0] + self.w3 * data[i][1] + self.b1) * data[i][1]
-                db0 = self.derivsigmoid(self, self.w0 * data[i][0] + self.w1 * data[i][1] + self.b0)
-                db1 = self.derivsigmoid(self, self.w2 * data[i][0] + self.w3 * data[i][1] + self.b1)
 
-                self.w0 -= q*dw0*dh0*deriv_mse
-                self.w1 -= q*dw1*dh0*deriv_mse
-                self.w2 -= q*dw2*dh1*deriv_mse
-                self.w3 -= q*dw3*dh1*deriv_mse
-                self.b0 -= q*db0*dh0*deriv_mse
-                self.b1 -= q*db1*dh1*deriv_mse
-                self.w4 -= q*dw4*deriv_mse
-                self.w5 -= q*dw5*deriv_mse
-                self.b2 -= q*db2*deriv_mse
+                dw0 = self.derivsigmoid(self, sum_h0) * data[i][0]
+                dw1 = self.derivsigmoid(self, sum_h0) * data[i][1]
+                db0 = self.derivsigmoid(self, sum_h0)
 
-                if j%10 == 0:
-                    print(i," : ", result[i])
+                dw2 = self.derivsigmoid(self, sum_h1) * data[i][0]
+                dw3 = self.derivsigmoid(self, sum_h1) * data[i][1]
+                db1 = self.derivsigmoid(self, sum_h1)
 
+                self.w0 -= learn_rate * dw0 * dh0 * deriv_mse
+                self.w1 -= learn_rate * dw1 * dh0 * deriv_mse
+                self.b0 -= learn_rate * db0 * dh0 * deriv_mse
+
+                self.w2 -= learn_rate * dw2 * dh1 * deriv_mse
+                self.w3 -= learn_rate * dw3 * dh1 * deriv_mse
+                self.b1 -= learn_rate * db1 * dh1 * deriv_mse
+
+                self.w4 -= learn_rate * dw4 * deriv_mse
+                self.w5 -= learn_rate * dw5 * deriv_mse
+                self.b2 -= learn_rate * db2 * deriv_mse
+
+                result[i] = self.feedforward(self, data[i])
+                loss[i] = self.mse(self, target_value[i], result[i])
+            if  j%10==0:
+                print("epoch: ", j)
+                self.printall(self, data, result, loss)
 
 
 learn_rate = 0.1
 epochs = 1000
-data = [[0,0], [0,1], [1,0], [1,1]]
-ypred = [0,0,0,1]
+data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+target_value = [0, 1, 1, 1]
 
-Neuron.train(Neuron, data, ypred, learn_rate, epochs)
+Neuron.train(Neuron, data, target_value, learn_rate, epochs)
